df56f92183467f5129d245421329b440be6b4c46
vadog <46636314+vadog@users.noreply.github.com>
2020-08-14 13:01:57 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Mihir Kale");
+		System.out.println("I like playing videogames");
+		System.out.println("My favorite videogame right now is Valorant");
+		System.out.println("I plan to major in computer engineering");
+	}
+}
+
